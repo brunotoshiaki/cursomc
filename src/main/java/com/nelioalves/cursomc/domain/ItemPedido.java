3818be0cd45b9d2ac7bc66bafdbcadb8fe8798c3
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2019-06-03 23:31:05 -0300

    Endepoint /pedidos/{id} disponibilizado

--- src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
@@ -5,11 +5,14 @@ import java.io.Serializable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class ItemPedido implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
 
@@ -18,7 +21,7 @@ public class ItemPedido implements Serializable {
 	private Double preco;
 
 	public ItemPedido() {
-		super();
+
 	}
 
 	public ItemPedido(Pedido pedido, Produto produto, Double desconto, Integer quantidade, Double preco) {
@@ -30,12 +33,21 @@ public class ItemPedido implements Serializable {
 		this.preco = preco;
 	}
 
+	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
 
 	public Produto getProtudo() {
 		return id.getProduto();
+	}
+
+	public ItemPedidoPK getId() {
+		return id;
+	}
+
+	public void setId(ItemPedidoPK id) {
+		this.id = id;
 	}
 
 	public Double getDesconto() {
@@ -86,7 +98,5 @@ public class ItemPedido implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
 
 }
